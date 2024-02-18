54c719f5798adcf1c29349343f626aaf8af6e5d6
realdevvora <legitvoradev@gmail.com>
2024-02-18 07:58:32 -0500

    Final changes

--- vite-project/src/Searchbar.jsx
+++ vite-project/src/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import "./Searchbar.css"
 
 const SearchBar = ({ data, onSelectLocation }) => {
   const [filteredData, setFilteredData] = useState([]);
